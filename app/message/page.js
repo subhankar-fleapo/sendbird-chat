74cacf619f773ab33d6c2d530e1319ff06931e6e
Subhankar Roy <subhankar@fleapo.com>
2023-06-06 12:34:55 +0530

    added the group message code

---
+++ app/message/page.js
@@ -0,0 +1,10 @@
+import BasicGroupChannelSample from '@/components/BasicGroupChannelSample'
+import React from 'react'
+
+export default function page() {
+    return (
+        <>
+            <BasicGroupChannelSample />
+        </>
+    )
+}
